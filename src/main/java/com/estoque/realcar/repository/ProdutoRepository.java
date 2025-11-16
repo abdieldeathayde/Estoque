6d898c0b4c0809b3a0969c8f800ddf4a7a083f80
abdieldeathayde <abdieldeathayde67@gmail.com>
2025-11-15 22:56:29 -0300

    feat: adicionando importacao arquivo excel

---
+++ src/main/java/com/estoque/realcar/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.estoque.realcar.repository;
+
+import com.estoque.realcar.service.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}

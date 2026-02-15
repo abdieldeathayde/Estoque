679151d5acd20902806aad9955c8e39fdac0fc71
Abdiel Athayde <abdieldeathayde67@gmail.com>
2026-02-15 09:53:47 -0300

    correção sistema

--- src/main/java/com/estoque/realcar/ProdutosApplication.java
+++ src/main/java/com/estoque/realcar/ProdutosApplication.java
@@ -9,3 +9,53 @@ public class ProdutosApplication {
         SpringApplication.run(ProdutosApplication.class, args);
     }
 }
+
+Error starting ApplicationContext. To display the condition evaluation report re-run your application with 'debug' enabled.
+2026-02-15T09:13:40.901-03:00 ERROR 8208 --- [           main] o.s.b.d.LoggingFailureAnalysisReporter   : 
+
+***************************
+APPLICATION FAILED TO START
+***************************
+
+Description:
+
+Parameter 1 of constructor in com.estoque.realcar.security.JwtAuthenticationFilter required a single bean, but 2 were found:
+	- customUserDetailsService: defined in file [C:\Users\Athay\dev\Estoque\Estoque\target\classes\com\estoque\realcar\security\CustomUserDetailsService.class]
+	- userDetailsServiceImpl: defined in file [C:\Users\Athay\dev\Estoque\Estoque\target\classes\com\estoque\realcar\security\UserDetailsServiceImpl.class]
+
+
+Action:
+
+Consider marking one of the beans as @Primary, updating the consumer to accept multiple beans, or using @Qualifier to identify the bean that should be consumed
+
+
+package com.estoque.realcar.security;
+
+import com.estoque.realcar.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuário não encontrado"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
+
